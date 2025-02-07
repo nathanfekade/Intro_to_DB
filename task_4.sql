d3a7e13a848ab3252a0a4d94454d56d9a30816fb
nathanfekade <nathanfekade276@gmail.com>
2025-02-07 22:38:04 +0300

    question 4 answer

--- task_4.sql
+++ task_4.sql
@@ -1,3 +1,5 @@
 USE alx_book_store;
 
-SHOW COLUMNS FROM books;
+
+
+SHOW "INFORMATION_SCHEMA.COLUMNS", "SELECT", "COLUMN_NAME", "COLUMN_TYPE", "TABLE_SCHEMA = 'alx_book_store'", "TABLE_NAME = 'Books'" FROM books;
